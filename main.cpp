c3437883587a8ef69c9acbd9541b8873d4e309c7
Svetlana <lanatschka@yandex.ru>
2020-09-09 22:45:25 +0700

    change d4

--- main.cpp
+++ main.cpp
@@ -5,14 +5,11 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Duck *d1 = new MallardDuck();
-    d1->display();
-    d1->quack();
+    Duck *d4 = new MallardDuck();
+    d4->display();
+    d4->quack();
 
-    Duck *d2 = new RedHeadDuck();
-    d2->display();
-    d2->quack();
-    d2->swim();
+
 
     return a.exec();
 }
